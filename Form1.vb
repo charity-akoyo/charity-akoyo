5aed8d9be83cd893b0c507600ed330707c83b943
charity-akoyo <charityakoyo51@gmail.com>
2025-02-07 18:49:01 +0300

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,21 +1,65 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        MsgBox("Welcoming Form")
+    Private Sub Button1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Object Demonstration")
 
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        MsgBox("Load Object Demo")
+    Private Sub Button2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Load Picture")
 
     End Sub
 
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles MyBase.Click
+    Private Sub Button3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Display Test")
+
+    End Sub
+
+    Private Sub Button4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Sample Test")
+
+    End Sub
+
+    Private Sub Button5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Font")
+
+    End Sub
+
+    Private Sub Button6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Garamond")
+
+    End Sub
+
+    Private Sub Button7_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Magneto")
+
+    End Sub
+
+    Private Sub Button8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Tahoma")
+
+    End Sub
+
+    Private Sub Button9_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Font Style")
+
+    End Sub
+
+    Private Sub Button10_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Font color")
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
         MsgBox("Application Exit")
-
 
     End Sub
 End Class
