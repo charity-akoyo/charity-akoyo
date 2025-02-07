6d6d764c99879f5fa43d9e1ac66c96c82c15aa79
charity-akoyo <charityakoyo51@gmail.com>
2025-02-07 18:48:14 +0300

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Welcoming Form")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("Load Object Demo")
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles MyBase.Click
+        MsgBox("Application Exit")
+
+
+    End Sub
+End Class
